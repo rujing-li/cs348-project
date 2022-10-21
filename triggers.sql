e4919ae89875a10a7bb7c45e577a8cc003665121
Echo_K_Fate <echow12138@gmail.com>
2022-10-20 23:09:43 -0400

    trigger modify

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,11 @@
 CREATE TRIGGER AvailabilityChecck
-BEFORE INSERT OF availability ON Carpool 
+BEFORE INSERT ON Carpool 
 REFERENCING NEW ROW AS newCarpool 
-FOR EACH ROW
+FOR EACH ROW 
+    WHEN (newCarpool.availablity > ANY(SELECT max_seats 
+                                        FROM car
+                                        WHERE newCarpool.car_plate = car.plate_num))
+         SET newCarpool.availablity = (SELECT MIN(max_seats)
+                                        FROM car
+                                        WHERE new.Carpool.car_plate = car.plate_num) - 1;
     
